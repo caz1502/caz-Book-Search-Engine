a183fab4a465d8c67e7ce17d318d5703d7cb9cbe
Carol Shiers <carol.shiers@gmail.com>
2022-02-08 15:00:21 +1000

    updated ADD_user, login still not working but diff errror now

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  query me($email: String!) {
+    me(email: $email) {
+      username
+      email
+      savedBooks {
+        title
+        authors
+        description
+      }
+      bookCount
+    }
+  }
+`;
