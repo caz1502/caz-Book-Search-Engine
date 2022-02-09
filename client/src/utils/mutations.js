b65e3097d17cbe6188ad68fdd73ec648e35ef4c0
Carol Shiers <carol.shiers@gmail.com>
2022-02-09 10:56:58 +1000

    login working but save is not working

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -15,11 +15,11 @@ export const LOGIN_USER = gql`
 export const ADD_USER = gql`
   mutation addUser($email: String!, $password: String!, $username: String!) {
     addUser(email: $email, password: $password, username: $username) {
+      token
       user {
         username
         email
       }
-      token
     }
   }
 `;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 // updated
+
 const express = require("express");
 const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
 
-// apollo server
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
@@ -15,24 +15,22 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// when in production, serve client/build as static assets
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-// app.use(routes);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`Legend API server running on port ${PORT}!`);
-    console.log(
-      `Legend Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-    );
+    console.log(` Rockstar - API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
+    console.log(`Rockstar - Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
